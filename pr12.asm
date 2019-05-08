039534d11e29be807704ca317aaa0c7fa996e13c
hk2511 <strapkobrest@gmail.com>
2019-05-08 13:01:59 +0300

    правильное первое задание 12пр

--- pr12.asm
+++ pr12.asm
@@ -1,46 +1,91 @@
 .MODEL Tiny
 .STACK 100h
-
-.DATA
-x DW 10
-Message1 DW 'Hello i am Bill Gates too',13,10,'$'
-Message2 DW 'Hello i am Bill Gates too','$'
-
+.DATA 
+x dw 10 
+y dw 0fh
 .CODE 
-call ini_data
+mov ax, @data
+mov ds,ax 
+GOTOXY  MACRO   col, row
+        PUSH    AX
+        PUSH    BX
+        PUSH    DX
+        MOV     AH, 02h
+        MOV     DH, row
+        MOV     DL, col
+        MOV     BH, 0
+        INT     10h
+        POP     DX
+        POP     BX
+        POP     AX
+ENDM
+
+PRINT   MACRO   sdat
+LOCAL   next_char, s_dcl, printed, skip_dcl
+
+PUSH    AX      ; store registers...
+PUSH    SI      ;
+
+JMP     skip_dcl        ; skip declaration.
+        s_dcl DB sdat, 0
+
+skip_dcl:
+        LEA     SI, s_dcl
+        
+next_char:      
+        MOV     AL, CS:[SI]
+        CMP     AL, 0
+        JZ      printed
+        INC     SI
+        MOV     AH, 0Eh ; teletype function.
+        INT     10h
+        JMP     next_char
+printed:
+
+POP     SI      ; re-store registers...
+POP     AX      ;
+ENDM
+
+PRINTN   MACRO   sdat
+LOCAL   next_char, s_dcl, printed, skip_dcl
+
+PUSH    AX      ; store registers...
+PUSH    SI      ;
+
+JMP     skip_dcl        ; skip declaration.
+        s_dcl DB sdat, 13, 10, 0
+
+skip_dcl:
+        LEA     SI, s_dcl
+        
+next_char:      
+        MOV     AL, CS:[SI]
+        CMP     AL, 0
+        JZ      printed
+        INC     SI
+        MOV     AH, 0Eh ; teletype function.
+        INT     10h
+        JMP     next_char
+printed:
+
+POP     SI      ; re-store registers...
+POP     AX      ;
+ENDM  
 
 mov cx, x
 line:
-call  string_in_line
-loop line
+PRINT 'o '
+loop line 
+mov cx, y
 
-mov cx, x
-add cx, 5
-colmn:
-call  string_in_colmn
-loop colmn
+gotoxy 1,1    
+
+colonm:
+PRINTN 't'
+loop colonm
+
 
 mov ax, 4c00h
 int 21h
-
-string_in_colmn PROC 
-mov dx, offset Message1
-mov ah, 9h
-int 21h
-ret
-string_in_colmn endp 
-
-string_in_line PROC
-mov dx, offset Message2
-mov ah, 9h
-int 21h
-ret
-string_in_line endp
-
-ini_data PROC
-mov ax, @DATA
-mov ds, ax
-ret 
-ini_data endp 
 
 end 
